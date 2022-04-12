5de589cbf59939f9dbc54cc32ec4d83d3985db02
arneeder <arne.eder@gmail.com>
2022-04-12 13:53:07 +0200

    final

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a,b) {
+      return a-b
+    })
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length) return this.items[pos]
+    else throw new Error('OutOfBounds')
+  }
 
-  max() {}
+  max() {
+    if (this.length !== 0) return this.items[this.length - 1]
+    else throw new Error('EmptySortedList');
+  }
 
-  min() {}
+  min() {
+    if (this.length !== 0) return this.items[0]
+    else throw new Error('EmptySortedList');
+  }
 
-  sum() {}
+  sum() {
+    const sum = this.items.reduce(function(acc, val) {
+      return acc += val
+    },0)
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if (this.length !== 0) return this.sum() / this.length
+    else throw new Error('EmptySortedList'); 
+  }
 }
 
 module.exports = SortedList;
